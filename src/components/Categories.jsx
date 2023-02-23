299f67bbbb2d6f7364a5dfba43c8310a742abfa2
Alexandr Borodinov <yaborodinov@gmail.com>
2023-02-23 22:27:42 +0200

    ability to chage categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 
 const Categories = () => {
+
+    const [activeElement, setActiveElement] = React.useState(0);
+
+    const categories = [
+        "All",
+        "Meat",
+        "Vegan",
+        "Grill",
+        "Spicy",
+        "Closed"
+    ];
+
+    const setActive = (index) => {
+        setActiveElement(index);
+    }
+    
     return (
         <div className="categories">
             <ul>
-                <li className="active">All</li>
-                <li>Meat</li>
-                <li>Vegan</li>
-                <li>Grill</li>
-                <li>Spicy</li>
-                <li>Closed</li>
+                { categories.map((value, index) => <li 
+                    className={activeElement === index ? 'active' : ''}
+                    onClick={() => setActive(index)}
+                    key={value + "_" + index}
+                >{value}</li>)}
             </ul>
         </div>
     )
-}
+};
 
 export default Categories;
