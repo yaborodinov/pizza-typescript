1201de4bf8c2b5329964e7538d38bcf598bab267
Alexandr Borodinov <yaborodinov@gmail.com>
2023-02-23 11:44:50 +0200

    separated the components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <div className="categories">
+            <ul>
+                <li className="active">All</li>
+                <li>Meat</li>
+                <li>Vegan</li>
+                <li>Grill</li>
+                <li>Spicy</li>
+                <li>Closed</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Categories;
